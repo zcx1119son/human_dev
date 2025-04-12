26e75e153e709ea2e7badc93491ae55da33c735c
hyunwoo <hyunwoo_9800@naver.com>
2025-04-12 15:30:58 +0900

    메인화면 임시 2025/04/12 @5.0h

---
+++ com/human/default_health/js/goPage.js
@@ -0,0 +1,14 @@
+// 페이지 이동용 js 파일
+
+// 헤더 로고 클릭 시 메인 이동용
+function goMain() {
+
+    window.location.href = "../html/index.html";
+
+}
+
+function goLogin() {
+
+    window.location.href = "../html/index.html";
+
+}
