0db48c1801745f9cd216f5e059f91e8cffb31171
hyunwoo <hyunwoo_9800@naver.com>
2025-04-14 21:21:19 +0900

    관리자 페이지 추가(작업중)

---
+++ com/human/default_health/js/goPageAdmin.js
@@ -0,0 +1,14 @@
+// 페이지 이동용 js 파일
+
+// 헤더 로고 클릭 시 메인 이동용
+function goMain() {
+
+    window.location.href = "../index.html";
+
+}
+
+function goLogin() {
+
+    window.location.href = "../user/login.html?login";
+
+}
