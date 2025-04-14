31dfe60f7e1dfac37b6788cb3ccba472e47f1047
hyunwoo <hyunwoo_9800@naver.com>
2025-04-14 21:21:06 +0900

    헤더 파일 이미지 경로 수정 및 메인 화면 css 경로 수정

--- com/human/default_health/js/goPage.js
+++ com/human/default_health/js/goPage.js
@@ -3,30 +3,36 @@
 // 헤더 로고 클릭 시 메인 이동용
 function goMain() {
 
-    window.location.href = "../html/index.html";
+    location.href = "../html/index.html";
 
 }
 
 function goLogin() {
 
-    window.location.href = "../html/user/login.html?login";
+    location.href = "../html/user/login.html?login";
 
 }
 
 function searchId() {
 
-    window.location.href = "../html/user/login.html?searchId";
+    location.href = "../html/user/login.html?searchId";
 
 }
 
 function searchPw() {
 
-    window.location.href = "../html/user/login.html?searchPw";
+    location.href = "../html/user/login.html?searchPw";
 
 }
 
 function goJoin() {
 
-    window.location.href = "../html/user/login.html?goJoin";
+    location.href = "../html/user/login.html?goJoin";
+
+}
+
+function goBack() {
+
+    history.back();
 
 }
